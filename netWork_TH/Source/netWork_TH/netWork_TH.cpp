1b9deed123c32b6c699dd35ccc8830ea71dbfd7d
Kaizn670 <hoangdan100@outlook.com>
2022-04-13 01:11:55 -0400

    deleted network_th not needed

--- netWork_TH/Source/netWork_TH/netWork_TH.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "netWork_TH.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, netWork_TH, "netWork_TH" );
